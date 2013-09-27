d5bcdc0867819cf427c56bf60814f56377dd463b
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 15:09:50 -0500

    fix index

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,10 +3,46 @@ get '/' do
   erb :index
 end
 
+get "/create/survey" do 
+
+  erb :create_survey
+end
+
+get '/profile/name' do
+
+  erb :profile
+end 
+
+get '/signup' do 
+
+  erb :signup
+end
+
+get '/surveys' do 
+
+  erb :surveys
+end
+
+get '/survey/1' do 
+
+  erb :survey
+end
+
 ###--------------------POST
 
 post '/login' do
 
-  redirect to "/"
+  redirect to "/surveys"
+end
+
+post '/signup' do 
+
+  redirect to '/surveys'
+end
+
+post '/create/survey/1' do 
+
+
+  redirect to '/surveys'
 end
 
