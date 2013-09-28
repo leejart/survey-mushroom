cacddccf265dad14f5fb25fb205b9f472e972618
Eigil Sagafos <eigil@sagafos.no>
2013-09-28 13:02:39 -0500

    changes to home page and css

---
+++ app/helpers/svg.rb
@@ -0,0 +1,13 @@
+helpers do
+
+  def icon(file)
+    # exec('pwd')
+    content = File.read("public/images/svg/#{file}.svg")
+  end
+
+  def image(file)
+    # exec('pwd')
+    content = File.read("public/images/svg/#{file}.svg")
+  end
+
+end
